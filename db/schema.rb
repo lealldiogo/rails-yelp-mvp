f57b70972d27e237fbd08bc35e7062f14e824c58
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-02-15 12:29:52 -0200

    Just created the reviews controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215141059) do
+ActiveRecord::Schema.define(version: 20170215142201) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -24,8 +24,10 @@ ActiveRecord::Schema.define(version: 20170215141059) do
   create_table "reviews", force: :cascade do |t|
     t.string   "content"
     t.integer  "rating"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
